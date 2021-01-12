9a7771cdd0b8cc77fadf04f588afe8f7b94603e4
mheriyanto <mheriyanto37@gmail.com>
2021-01-12 19:23:01 +0700

    [test] test torch

--- README.md
+++ README.md
@@ -8,15 +8,22 @@ Repository for playing the computer vision apps: **face recognition** on Raspber
 + 5M USB Retractable Clip 120 Degrees WebCam Web Wide-angle Camera Laptop U7 Mini or [Raspi Camera](https://www.raspberrypi.org/documentation/hardware/camera/)
 
 ### Software
-+ [Raspbian Buster 32 bit](https://downloads.raspberrypi.org/raspios_armhf/images/raspios_armhf-2020-12-04/2020-12-02-raspios-buster-armhf.zip)), install on RasberriPi 3
++ [Raspbian Buster 32 bit](https://downloads.raspberrypi.org/raspios_armhf/images/raspios_armhf-2020-12-04/2020-12-02-raspios-buster-armhf.zip), install on RasberriPi 3
 + PyTorch: torch 1.6.0 [armv7l](https://github.com/ljk53/pytorch-rpi/blob/master/torch-1.6.0a0%2Bb31f58d-cp37-cp37m-linux_armv7l.whl) and torchvision 0.7.0 [armv7l]()
 + Python min. ver. 3.6
+
+### Install the prerequisites 
+```console
+$ sudo apt install build-essential make cmake git python3-pip libatlas-base-dev
+$ sudo apt install libssl-dev
+$ sudo apt install libopenblas-dev libblas-dev m4 cython python3-yaml
+```
 
 ## Getting Started
 + 
 
 # Credit to
-+ 
++ [Share PyTorch binaries built for Raspberry Pi](https://github.com/ljk53/pytorch-rpi)
 
 ## Reference
 + 
---
+++ test/test_torch.py
@@ -0,0 +1,70 @@
+# Ref: https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/01-basics/pytorch_basics/main.py
+
+# === TODO: 1. Basic autograd example 1  
+
+import torch 
+import torch.nn as nn
+import numpy as np
+
+# Create tensors.
+x = torch.tensor(1., requires_grad=True)
+w = torch.tensor(2., requires_grad=True)
+b = torch.tensor(3., requires_grad=True)
+
+# Compute gradients.
+y.backward()
+
+# Print out the gradients.
+print(x.grad)    # x.grad = 2 
+print(w.grad)    # w.grad = 1 
+print(b.grad)    # b.grad = 1 
+
+# Create tensors of shape (10, 3) and (10, 2).
+x = torch.randn(10, 3)
+y = torch.randn(10, 2)
+
+# === TODO: 2. Basic autograd example 2   
+# Create tensors of shape (10, 3) and (10, 2).
+x = torch.randn(10, 3)
+y = torch.randn(10, 2)
+
+# Build a fully connected layer.
+linear = nn.Linear(3, 2)
+print ('w: ', linear.weight)
+print ('b: ', linear.bias)
+
+# Build loss function and optimizer.
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(linear.parameters(), lr=0.01)
+
+# Forward pass.
+pred = linear(x)
+
+# Compute loss.
+loss = criterion(pred, y)
+print('loss: ', loss.item())
+
+# Backward pass.
+loss.backward()
+
+# Print out the gradients.
+print ('dL/dw: ', linear.weight.grad) 
+print ('dL/db: ', linear.bias.grad)
+
+# 1-step gradient descent.
+optimizer.step()
+
+# Print out the loss after 1-step gradient descent.
+pred = linear(x)
+loss = criterion(pred, y)
+print('loss after 1 step optimization: ', loss.item())
+
+# === TODO: 3. Loading data from numpy
+# Create a numpy array.
+x = np.array([[1, 2], [3, 4]])
+
+# Convert the numpy array to a torch tensor.
+y = torch.from_numpy(x)
+
+# Convert the torch tensor to a numpy array.
+z = y.numpy()
